3e582a5ff58657f0f7a3cce4a454183af8b4ea57
Gupta-Anubhav12 <anubhavgupta2260@gmail.com>
2023-02-24 14:31:35 +0530

    wip embeddings

--- Vectorise_Script/embedding.py
+++ Vectorise_Script/embedding.py
@@ -1,33 +1,61 @@
 import json
 import openai
+import tiktoken
+import os
+from tenacity import retry,wait_fixed
+
+tokenizer = tiktoken.get_encoding("gpt2")
+
+
+
 openai.api_key = ""
+
+@retry(wait=wait_fixed(2))
 def get_embedding(text, model="text-embedding-ada-002"):
-    text = text.replace("\n", " ")
+    print("embedding")
     return openai.Embedding.create(input=[text], model=model)["data"][0]["embedding"]
 
-for i in range(1,20): #19 chapters in Canto 1 
-    with open('chapter'+str(i)+'.json', encoding='utf-8') as fh:
-        dataset = json.load(fh)
-    embeddings = []
-    for verse in dataset:
-        emb = {
-            "id": verse["verse"],
-            "devnagari":verse["devanagari"],
-            "chapter_number": "1",
-            "english_devnagari": verse["english_devnagari"],
-            "verse_Syn": verse["verse_Syn"],
-            "translation":verse["translation"],
-            "purport": verse["purport"],
-            "embedding": None
-        }
-        purport = verse["purport"]
-        chapter_number = "1"
-        verse_number = verse["verse"]
-        translation = verse["translation"]       
-                
-        text = f"Bhagavad Gita: Chapter {chapter_number}, Verse {verse_number},{translation},{purport}"
-        emb["embedding"] = get_embedding(text)
-        embeddings.append(emb)
 
-    with open('chapter'+str(i)+'_vectorised.json', "w", encoding="utf-8") as f:
-        json.dump(embeddings, f, ensure_ascii=False, indent=4)
+
+
+canto_path = "srimad-bhagavatam/With Purport/"
+vectorized_canto_path = "srimad-bhagavatam/vectorized/"
+tokens = 0
+for canto_number in range(5,13): #12 cantos in Srimad Bhagavatam
+    print("enterint canto",canto_number)
+    for file in os.listdir(f"{canto_path}/Canto {canto_number}"):
+        print(file)
+        with open(f'{canto_path}/Canto {canto_number}/{file}', encoding='utf-8') as fh:
+            dataset = json.load(fh)
+        embeddings = []
+        for verse in dataset:
+            chapter_number = file.replace(".json","").replace("Chapter","")
+            emb = {
+                "id": verse["verse"],
+                "devnagari":verse["devanagari"],
+                "chapter_number": chapter_number,
+                "english_devnagari": verse["english_devnagari"],
+                "verse_Syn": verse["verse_Syn"],
+                "translation":verse["translation"],
+                "purport": verse["purport"],
+                "embedding": None
+            }
+            purport = verse["purport"]
+            
+            verse_number = verse["verse"]
+            translation = verse["translation"]     
+            print("canto",canto_number,"chapter",chapter_number,"verse",verse_number)  
+                    
+            text = f"Shrimd Bhagavatam: Canto {canto_number} Chapter {chapter_number}, Verse {verse_number},{translation},\n purport {purport}"
+
+            tokens += len(tokenizer.encode(text))
+
+            emb["embedding"] = get_embedding(text)
+            embeddings.append(emb)
+
+        with open(f'{vectorized_canto_path}/canto_{canto_number}/{file}', "w", encoding="utf-8") as f:
+            json.dump(embeddings, f, ensure_ascii=False, indent=4)
+
+
+print("tokens --->",tokens)
+print("cost --->",(tokens/1000)*0.0004)
